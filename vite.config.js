1bf02032954a455a07b6c7334f0c08a6393f6f10
Sburet42 <frifano.gamer@gmail.com>
2026-02-01 23:32:27 +0100

    update vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: './', // Assurez-vous que cette ligne est présente pour un déploiement statique
+  build: {
+    outDir: 'dist', // Dossier de sortie
+    emptyOutDir: true, // Vider le dossier avant chaque build
+  },
+});
